c623ed16b9ea66598efb562ef8359583c203a188
hichammg4 <58368209+hichammg4@users.noreply.github.com>
2025-05-15 23:13:36 +0100

    add first migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,11 @@ import type { Config } from "drizzle-kit"
 export default {
   schema: "./lib/db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
+  },
+  migrations: {
+    schema: 'public',
   },
 } satisfies Config
