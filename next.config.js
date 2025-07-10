599c5abcaf78b3f607ecd47e4757985854c10b6e
Bakary Konate <boukymen@gmail.com>
2025-07-10 12:31:24 +0000

    MM-1414: build fix 2.1

--- next.config.js
+++ next.config.js
@@ -31,6 +31,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Disable ESLint during build
   },
+  pageExtensions: [
+    "page.tsx",
+    "page.ts",
+    // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
+    // Instead, use `not-found.ts` as a workaround
+    // "ts" is required to resolve `not-found.ts`
+    // https://github.com/vercel/next.js/issues/65447
+    "ts"
+  ],
   env: {
     NEXT_PUBLIC_APP_VERSION: packageJson.version,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
