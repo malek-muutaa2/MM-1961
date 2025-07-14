c04a00b699ecaaccde589042a842b04efedd7fe9
Bakary Konate <boukymen@gmail.com>
2025-07-14 17:48:09 +0000

    MM-1414: fix sonar check

--- jest.config.ts
+++ jest.config.ts
@@ -15,6 +15,14 @@ const config = {
 },
   // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    // Use babel-jest for all JS files (including node_modules/jose)
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    // Explicitly allow transpilation of `jose` (ignore other node_modules)
+    "/node_modules/(?!(jose|@panva|next-auth)/)",
+  ],
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
