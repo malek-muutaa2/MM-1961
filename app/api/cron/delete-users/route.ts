2db920f5fa927d1a23fa2e15c9208374e17a1c45
hichammg4 <58368209+hichammg4@users.noreply.github.com>
2025-08-28 17:55:34 +0100

    update

--- app/api/cron/delete-users/route.ts
+++ app/api/cron/delete-users/route.ts
@@ -2,5 +2,7 @@ import { permanentlyDeleteUsers } from "@/lib/delete_users";
 
 export async function GET() {
   await permanentlyDeleteUsers();
+  console.log("Old users deleted");
+  
   return Response.json({ message: "Old users deleted" });
 }
